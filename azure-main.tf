6382dfb93a37e8d61eee2bf3083fd08e6236979d
Kristian Marstrander <kristian@marstrander.com>
2024-10-23 14:11:29 +0200

    formatted terraform files

--- azure-main.tf
+++ azure-main.tf
@@ -12,7 +12,7 @@ terraform {
     storage_account_name = "iactfexampleacc"                       # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "terraform.tfstate"                     # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-   use_oidc = true
+    use_oidc             = true
   }
 }
 

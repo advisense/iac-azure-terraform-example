2730df9af04edffcf8306f8a5c9d4b2578a4d8b4
Arne Berner <aberner@practical.no>
2025-02-12 14:14:47 +0100

    removed one of the desktop's

--- spoke2.tf
+++ spoke2.tf
@@ -50,37 +50,3 @@ module "desktop-windows-01" {
   depends_on         = [module.workstations-vnet]
 }
 
-
-# a second virtual desktop server
-module "desktop-windows-02" {
-  source                        = "./modules/vm-windows"
-  virtual_machine_name          = "microsavevm02"
-  nic_name                      = "vm02-nic"
-  location                      = module.internal-workstation-resourcegroup.rg_location
-  resource_group_name           = module.internal-workstation-resourcegroup.rg_name
-  ipconfig_name                 = "ipconfig2"
-  subnet_id                     = module.workstations-vnet.vnet_subnet_id[0]
-  private_ip_address_allocation = "Static"
-  private_ip_address            = "10.52.1.5"
-  vm_size                       = "Standard_B2s"
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  delete_os_disk_on_termination = true
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  delete_data_disks_on_termination = true
-
-  publisher       = "MicrosoftWindowsDesktop"
-  offer           = "windows-11"
-  sku             = "win11-23h2-pro"
-  storage_version = "latest"
-
-  os_disk_name      = "wm02osdisk"
-  caching           = "ReadWrite"
-  create_option     = "FromImage"
-  managed_disk_type = "Standard_LRS"
-
-  admin_username = "microsave.admin"
-  admin_password = var.admin_password
-
-  provision_vm_agent = true
-  depends_on         = [module.workstations-vnet]
-}
